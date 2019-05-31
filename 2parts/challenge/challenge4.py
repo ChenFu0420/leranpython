091801d853a58f38417c93e9a02b4009a1014ee9
chenfu <hiziying@hotmail.com>
2019-05-31 15:08:59 +0800

    change name 2day->2parts

---
+++ 2parts/challenge/challenge4.py
@@ -0,0 +1,5 @@
+num = int(input("输入一个数"))
+print("十进制：%d" % num)
+print("八进制：%o" % num)
+print("十六进制：%X" % num)
+print("二进制：", bin(num) )
---
+++ 2parts/in_test.py
@@ -0,0 +1,5 @@
+s = 'crazyit.org'
+print('it' in s)
+print('it' not in s)
+print('fkit' in s)
+print('f' not in s)
